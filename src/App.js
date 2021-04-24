79f86865e2e780913d4cf683529d3bfda8ae39c6
Rahini <rahini77@gmail.com>
2021-04-25 02:17:12 +0530

    cra messenger app files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,143 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from "react";
+import { Box, Paper, TextareaAutosize, Typography, Switch } from '@material-ui/core';
+import logo from './images/logo-cognite.png'
 import './App.css';
 
 function App() {
+  const [userMessages, setUserMessages] = useState([]);
+  const [clientMessages, setClientMessages] = useState(['Hi..', 'How are you, buddy?']); // Hard coding old chat messages from friend
+  const [newMessage, setNewMessage] = useState('');
+
+  const messageInput = useRef(null);
+  const [state, setState] = React.useState({
+    checkedEnter: true,
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    submitMessage();
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setNewMessage(e.target.value)
+  }
+
+  const handleToggleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+  const submitMessage = () => {
+    newMessage && setUserMessages(state => [...state, newMessage.trim()]);
+    messageInput.current.focus(); // setting focus back to the field where user types
+    setNewMessage('');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo}
+          alt="logo"
+          style={{ height: '6rem' }} />
+
+
       </header>
-    </div>
+      <Typography variant="h4" style={{ textAlign: 'center', color: '#45B8AC', margin: '1rem auto' }}>Cognite Messenger App</Typography>
+
+      <Switch
+        checked={state.checkedEnter}
+        onChange={handleToggleChange}
+        name="checkedEnter"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      Press 'Enter' key to send message
+
+      <div className="Content-container" style={{
+        margin: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        border: '4px solid #45B8AC'
+      }}>
+
+
+        {clientMessages.length > 0 && (
+          <Box style={{
+            float: 'left', margin: '1rem', display: 'flex', flexDirection: 'column',
+            gap: '1rem',
+          }}>
+
+            {clientMessages.map((cm, index) => (
+
+              <Box key={index} style={{ width: 'fit-content' }}>
+
+                <Paper elevation={3} style={{ padding: '0.5rem', border: '2px solid #EFC050' }}
+                >
+                  <i className="fa fa-user-circle-o" aria-hidden="true" style={{ color: '#EFC050' }}></i>
+                  <br></br>
+                  {cm}
+                </Paper>
+              </Box>
+            ))}
+
+          </Box>
+
+        )}
+
+        <Box className="User-message-box" style={{
+
+          float: 'right', margin: '1rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          alignItems: 'flex-end'
+        }}>
+          {userMessages.map((um, index) => (
+            <Box key={index} style={{
+              width: 'fit-content',
+              float: 'right',
+            }}>
+              <Paper elevation={3} style={{
+                padding: '0.5rem', border: '2px solid #9B2335',
+              }}
+              >
+                <i className="fa fa-user-circle-o" aria-hidden="true" style={{ color: '#9B2335' }}></i>
+                <br></br>
+                {um}
+              </Paper>
+            </Box>
+          ))}
+
+
+        </Box>
+
+        <Box style={{ margin: '2rem', }}>
+
+
+          <TextareaAutosize aria-label="empty textarea" placeholder=""
+            onChange={handleChange}
+            onKeyPress={(e) => {
+              state.checkedEnter && newMessage && e.key === 'Enter' && submitMessage();
+            }}
+            value={newMessage}
+            ref={messageInput}
+            style={{
+
+              padding: '0.5rem',
+              width: '90%',
+              textAlign: 'center',
+            }}
+          />
+
+          <span onClick={handleClick} style={{
+            cursor: 'pointer'
+          }}>
+            <i className="fa fa-paper-plane" style={{ margin: 'auto 1rem', color: '#45B8AC' }}
+              aria-hidden="true">
+            </i>
+          </span>
+        </Box>
+
+      </div>
+    </div >
   );
 }
 
